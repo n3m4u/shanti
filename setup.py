246cd47574b66b39937e098eb561d58c8c94b4f2
Nara Marella <atmp.help@gmail.com>
2025-04-29 19:35:07 +0200

    v0.1.0

--- setup.py
+++ setup.py
@@ -1,20 +1,8 @@
 from setuptools import setup, find_packages
-import os
-import re
-
-# Read version from version.py without importing the package
-with open(os.path.join('shanti', 'version.py'), 'r') as f:
-    version_file = f.read()
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
-    if version_match:
-        version = version_match.group(1)
-    else:
-        raise RuntimeError("Unable to find version string in version.py")
-
 
 setup(
     name="shanti",
-    version=version,
+    version=0.1.0,
     description="create SHarable, interactive, stANdalone html dashboard from Tabular proteomIcs data",
     author="Nara Marella",
     packages=find_packages(),
